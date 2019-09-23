03c6409f76f32430fe142df196e14c7b88c5c6e2
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-09-24 00:59:38 +0800

    Add project repository simple instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,46 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:freelancer_jobs_showcase/core/repositories/project_repository.dart';
 import 'package:freelancer_jobs_showcase/ui/screens/home_screen.dart';
 
-void main() => runApp(FreelancerJobsShowcaseApp());
+class FLBlocDelegate extends BlocDelegate {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print(error);
+  }
+}
+
+main() {
+  BlocSupervisor.delegate = FLBlocDelegate();
+  final _projectRepository = ProjectRepository();
+  runApp(FreelancerJobsShowcaseApp(
+    projectRepository: _projectRepository,
+  ));
+}
 
 class FreelancerJobsShowcaseApp extends StatelessWidget {
+  final projectRepository;
+
+  const FreelancerJobsShowcaseApp({Key key, this.projectRepository})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,7 +48,9 @@ class FreelancerJobsShowcaseApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: HomeScreen(
+        projectRepository: projectRepository,
+      ),
     );
   }
 }
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -3,8 +3,13 @@
 // license that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:freelancer_jobs_showcase/core/repositories/project_repository.dart';
 
 class HomeScreen extends StatefulWidget {
+  final ProjectRepository projectRepository;
+
+  const HomeScreen({Key key, this.projectRepository}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return HomeScreenState();
@@ -12,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  ProjectRepository get _projectRepository => widget.projectRepository;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
